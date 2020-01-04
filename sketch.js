f35c6f2cc9f0d5b6be0bd471929d995b03be4b1a
generative-exploration <59022519+generative-exploration@users.noreply.github.com>
2020-01-04 16:51:26 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+let img;
+  let savedMouse;
+  let auswahlX = 250;
+  let auswahlY = 180;
+
+
+function preload(){ 
+  img = loadImage("0095.jpg");
+  myFont = loadFont('FiraCode-Regular.ttf');
+}
+
+function setup() {
+  let c =  createCanvas(960, 1200);
+  image(img, 0, 0);
+  textFont(myFont);
+  textSize(14);
+//  frameRate(10);
+  savedMouse = createVector(0, 0);
+}
+
+function draw() {
+  copy(img, 0, height-20, width, height, 0, height-20, width, height);  
+   
+  copy(img, auswahlX, auswahlY, 150, 150, mouseX, mouseY, 150, 150);
+   
+  fill(255);
+  textAlign(CENTER);
+  text("bodypart: "+ key , width/2, height-5);
+}
+
+function mousePressed() {
+    savedMouse.x = mouseX; //Save the mouse position;
+    savedMouse.y = mouseY;
+  }
+
+function keyPressed() { 
+   if (key === 's') {
+      saveCanvas(c, 'gen_exploration', 'jpg');
+  }
+  
+  if (key == 'r') {
+    image (img, 0, 0);
+  }
+
+     if (key == "1") {
+      auswahlX = 250;
+      auswahlY = 180;
+    }
+    if (key == "2") {
+      auswahlX = 300;
+      auswahlY = 300;
+    }
+    if (key == "3") {
+      auswahlX = 325;
+      auswahlY = 540;
+    }
+    if (key == "4") {
+      auswahlX = 290;
+      auswahlY = 700;
+    }
+  }
